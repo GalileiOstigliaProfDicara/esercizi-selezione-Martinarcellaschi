17946ab32432a71e96536e72d98762ad4b89d109
Martinarcellaschi <martina.arcellaschi@galileiostiglia.edu.it>
2026-04-17 09:00:29 +0000

    .

---
+++ arrey1.c
@@ -0,0 +1,17 @@
+/*Chiedere all'utente e memorizzare in un arrey di 8 celle, 8 numeri reali ì, dopo averli memorizzati calcolare la somma e la media*/
+
+#include <stdio.h>
+
+int main (){
+    float arrey[8], somma, media; 
+
+    for (int i = 0; i < 8; i++){
+        printf ("Inserisci un numero reale: "); 
+        scanf ("%f", &arrey[i]); 
+        somma = somma + arrey[i]; 
+    }
+
+    media = somma / 8; 
+    printf ("La somma dei valori che hai inserito è %.2f\n", somma); 
+    printf ("La media dei valori che hai inserito è %.2f\n", media); 
+}
